a6a992714aba8f1a67909e0b49b946c7dc6f8100
Genaro Nieto <gnieto@uoc.edu>
2019-11-01 01:19:58 +0100

    Cambio la dirección IP de escucha

--- EchoSrv/echosrv.py
+++ EchoSrv/echosrv.py
@@ -1,7 +1,7 @@
 import sys
 import socket
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+HOST = '10.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 7			# Port to listen on (non-privileged ports are > 1023)
 
 #with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
