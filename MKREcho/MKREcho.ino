e244d87c28a74dd35ce3cbbb01bcb8d7b29a4e77
Genar Nieto <gnieto@uoc.edu>
2019-11-02 03:29:08 +0100

    Versión con lectura de parámetros y texto por consola

--- MKREcho/MKREcho.ino
+++ MKREcho/MKREcho.ino
@@ -1,29 +1,12 @@
-/*
-
-  This example connects to a encrypted WiFi network (WPA/WPA2).
-  Then it prints the  MAC address of the WiFi shield,
-  the IP address obtained, and other network details.
-  Then it continuously pings given host specified by IP Address or name.
-
-  Circuit:
-   WiFi shield attached / MKR1000
-
-  created 13 July 2010
-  by dlf (Metodo2 srl)
-  modified 09 June 2016
-  by Petar Georgiev
-*/
-#include <SPI.h>
 #include <WiFi101.h>
 
-#include "arduino_secrets.h" 
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = "Genaro0712";        // your network SSID (name)
-char pass[] = "passw0rd";    // your network password (use for WPA, or use as key for WEP)
-int status = WL_IDLE_STATUS;     // the WiFi radio's status
+char ssid[16]; // SSID
+char pwd[16];  // Password
+int status = WL_IDLE_STATUS;     
 
-// Specify IP address or hostname
-char host[] = "10.0.0.1";
+// Especificamos la ubicación del servdidor ECHO
+//char host[] = "10.0.0.1";
+char host[] = "192.168.101";
 byte port = 7;
 
 WiFiClient client;
@@ -31,116 +14,121 @@ WiFiClient client;
 void setup() {
   // Initialize serial and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  while (!Serial);
 
-  // check for the presence of the shield:
+  // Verificamos que la Ethernet está disponible.
+  // En una MKR1000 nunca debería fallar.
   if (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("WiFi shield not present");
-    // don't continue:
     while (true);
   }
 
-  // attempt to connect to WiFi network:
-  while ( status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
+  readSSID(ssid,pwd);
 
-    // wait 5 seconds for connection:
+  // Nos conectamos al AP (Raspberry)
+  while ( status != WL_CONNECTED) {
+    Serial.print(".");
+    status = WiFi.begin(ssid, pwd);
+
+    if (status == WL_CONNECT_FAILED || status == WL_DISCONNECTED) {
+      Serial.println("Invalid SSID or Password.");
+      readSSID(ssid,pwd);
+    }
+
+    // Espero un poco antess de reintentar la conexión
     delay(5000);
   }
+  Serial.println("\nConectado.");
 
-  // you're connected now, so print out the data:
-  Serial.println("You're connected to the network");
-  printCurrentNet();
-  printWiFiData();
-
-  Serial.print("Pinging ");
-  Serial.print(host);
-  Serial.print(":");
-  Serial.println(port);
-
-//  if (client.connect(host,port)) {
-//    client.print("Genaro es el mejor.");
-//  }
+  dumpWiFi();
 }
 
 void loop() {
+  char data[128];
 
+  readValue("\nMessage?",data);
+  echo(data);
+}
+
+// Enviamos un mensaje al servidor ECHO y esperamos 
+// a recibir la respuesta
+// TODO: Añadir un timeout a la espera de la respuesta
+void echo(char *message) {
+
+  // Conectamos con el servidor ECHO
   if (client.connect(host,port)) {
-    client.println("Genaro es el mejor.\n");
+    // Enviamos el mensaje
+    Serial.print("Sending:");
+    Serial.println(message);
+    client.println(message);
+
+    // Esperamos a que llegue la respuesta
     while (!client.available()) {
       delay(50);
       Serial.print(".");
     }
     Serial.println("");
-    if (client.available()>0) {
+    
+    // Leemos cada uno de los caracteres de la conexión y 
+    // los volcamos por pantalla
+    Serial.print("Receiving:");
       while (client.available()) {
         char c = client.read();
         Serial.write(c);
       } 
-    }
+    Serial.println("");
 
+    // Cerramos la conexión
     client.stop();
   }
-
-  delay(5000);
 }
 
-void printWiFiData() {
-  // print your WiFi shield's IP address:
+// Vuelco información básica de la conexión
+void dumpWiFi() {
+
+  // Imprimo la SSID de la conexión
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // Imprimo la configuración de red
   IPAddress ip = WiFi.localIP();
-  Serial.print("IP address : ");
+  Serial.print("IP address:  ");
   Serial.println(ip);
 
   Serial.print("Subnet mask: ");
   Serial.println((IPAddress)WiFi.subnetMask());
 
-  Serial.print("Gateway IP : ");
+  Serial.print("Gateway IP:  ");
   Serial.println((IPAddress)WiFi.gatewayIP());
 
-  // print your MAC address:
-  byte mac[6];
-  WiFi.macAddress(mac);
-  Serial.print("MAC address: ");
-  printMacAddress(mac);
 }
 
-void printCurrentNet() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+// Leo el SSD la contraseña
+void readSSID(char *ssid, char *pwd) {
+  readValue("SSID?",ssid);
+  readValue("Password?",pwd);
 
-  // print the MAC address of the router you're attached to:
-  byte bssid[6];
-  WiFi.BSSID(bssid);
-  Serial.print("BSSID: ");
-  printMacAddress(bssid);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI): ");
-  Serial.println(rssi);
-
-  // print the encryption type:
-  byte encryption = WiFi.encryptionType();
-  Serial.print("Encryption Type: ");
-  Serial.println(encryption, HEX);
-  Serial.println();
+  Serial.print("Conecting with SSID: ");
+  Serial.println(ssid);
 }
 
-void printMacAddress(byte mac[]) {
-  for (int i = 5; i >= 0; i--) {
-    if (mac[i] < 16) {
-      Serial.print("0");
-    }
-    Serial.print(mac[i], HEX);
-    if (i > 0) {
-      Serial.print(":");
-    }
+// Leo un parámetro de la consola
+// TODO: Validar que no llegan más caracteres de los esperados
+void readValue(char *msg, char *data) {
+
+  // Mostramos el mensaje
+  Serial.println(msg);
+  // Esperamos a que lleguen caracteres
+  while(!Serial.available()); 
+  
+  // Leemos los caracteres
+  int n = Serial.available();
+  for(int i=0; i<n; i++) {
+    data[i] = Serial.read();
   }
-  Serial.println();
+  
+  // Terminamos la cadena. 
+  // Sé que tendría que ser data[n] pero así evito el CR final.
+  data[n-1] = '\0'; 
+
 }
